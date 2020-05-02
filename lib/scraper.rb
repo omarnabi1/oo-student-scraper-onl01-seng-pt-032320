8083570f0c44830fc49f9933a2744b8bcfc93a7d
Omar Nabi <onabi20@gmail.com>
2020-05-02 20:59:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
-    flatiron_students = Array.new
+    flatiron_students = [ ]
     doc.css("div.roster-cards-container").each do |card|
       card.css(".student-card a").each do |student|
         profile_link = "#{student.attr('href')}"
