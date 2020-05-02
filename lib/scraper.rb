9bc8a96fada8450804c0db36a0b8339dbf26fe85
Omar Nabi <onabi20@gmail.com>
2020-05-02 20:58:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,8 +10,9 @@ class Scraper
     
     doc = Nokogiri::HTML(open(index_url))
       
-      doc.css('div.student-card').each {|student|
-      students << { 
+      doc.css("div.roster-cards-container").each do |card|
+        card.css("student-card a").each do |student|
+          
         :name => student.css('h4.student-name').text, 
         :location => student.css('p.student-location').text,
         :profile_url => "./fixtures/student-site/" + student.css('a').first.attr('href')
