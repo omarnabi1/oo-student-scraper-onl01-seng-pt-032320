6e8317d6f7b24873ce2afd25b3f4a54d6ba4ec38
Omar Nabi <onabi20@gmail.com>
2020-05-02 20:47:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,7 +12,8 @@ class Scraper
     students_list.css('div.student-card').each {|student|
       students << { 
         :name => student.css('h4.student-name').text, 
-        :location => student.css('p.student-location').text
+        :location => student.css('p.student-location').text,
+        :profile_url => 
     
   end
 
