3aaa454d38e1ea5c644c40a26ace57f3283cc2c7
Omar Nabi <onabi20@gmail.com>
2020-05-02 20:30:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,13 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    doc = Nokogiri::HTML(open(index_url))
+    flatiron_students = [ ]
+    
     
   end
 
