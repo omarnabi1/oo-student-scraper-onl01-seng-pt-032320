958dd6c73db6ebf31662ed704846e9b329ad1594
Omar Nabi <onabi20@gmail.com>
2020-05-02 20:48:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,9 +13,12 @@ class Scraper
       students << { 
         :name => student.css('h4.student-name').text, 
         :location => student.css('p.student-location').text,
-        :profile_url => 
-    
+        :profile_url => "./fixtures/student-site/" + student.css('a').first.attr('href')
+      }
+    }
+    students
   end
+    
 
   def self.scrape_profile_page(profile_url)
     
