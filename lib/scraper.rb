5ac2783dbb8f24c797e76716839775b1ea556334
Omar Nabi <onabi20@gmail.com>
2020-05-02 20:45:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,11 +3,16 @@ require 'pry'
 require 'nokogiri'
 
 class Scraper
+ 
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open(index_url))
-    flatiron_students = [ ]
-    doc.css("div.roster")
+     students = [ ]
+    
+    students_list = Nokogiri::HTML(open(index_url))
+    students_list.css('div.student-card').each {|student|
+      students << { 
+        :name => student.css('p.student-location').text 
+        :location => student.css('p')
     
   end
 
